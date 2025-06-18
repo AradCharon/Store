ea1c8c96ef51daf3cc8f3d5dfc5ed2503676b767
aradcharon <aradshafiee06@gmail.com>
2025-06-18 12:22:05 +0330

    adding BitcoinPayment class

---
+++ src/payment/BitcoinPayment.java
@@ -0,0 +1,20 @@
+package payment;
+
+class BitcoinPayment implements PaymentStrategy {
+    private String walletAddress;
+
+    public BitcoinPayment(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.printf("Payment of %.2f with Bitcoin to wallet address %s was completed.\n",
+                amount, walletAddress);
+    }
+
+    @Override
+    public String getPaymentDetails() {
+        return "Payment with Bitcoin - Wallet address: " + walletAddress;
+    }
+}
