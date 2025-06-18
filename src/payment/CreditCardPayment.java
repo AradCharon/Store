b0302de9632ed155fb91a492a739f3cda7239cd6
aradcharon <aradshafiee06@gmail.com>
2025-06-18 12:08:14 +0330

    adding CreditCardPayment class

---
+++ src/payment/CreditCardPayment.java
@@ -0,0 +1,23 @@
+package payment;
+
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+    private String cardHolderName;
+
+    public CreditCardPayment(String cardNumber, String cardHolderName) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.printf("Payment of %.2f with credit card number %s under name %s was processed.\n",
+                amount, cardNumber, cardHolderName);
+    }
+
+    @Override
+    public String getPaymentDetails() {
+        return "Payment with credit card - Card number: " + cardNumber +
+                " - Cardholder name: " + cardHolderName;
+    }
+}
