d1f1bdb4989f1ed76d4ba75c05833845b907fde2
aradcharon <aradshafiee06@gmail.com>
2025-06-18 12:16:38 +0330

    adding PayPalPayment class

---
+++ src/payment/PayPalPayment.java
@@ -0,0 +1,19 @@
+package payment;
+
+class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.printf("Payment of %.2f via PayPal account with email %s was processed.\n", amount, email);
+    }
+
+    @Override
+    public String getPaymentDetails() {
+        return String.format("Payment via PayPal - Email: %s", email);
+    }
+}
