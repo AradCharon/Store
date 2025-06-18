652c5af6d4da7bf813fef55e67cc31bdb3d30985
aradcharon <aradshafiee06@gmail.com>
2025-06-18 12:28:59 +0330

    adding Customer abstract class

--- src/payment/Customer.java
+++ src/payment/Customer.java
@@ -1,4 +1,29 @@
 package payment;
 
-public interface Customer {
+import java.util.ArrayList;
+
+abstract class Customer {
+    protected String name;
+    protected ArrayList<String> paymentHistory;
+
+    public Customer(String name) {
+        this.name = name;
+        this.paymentHistory = new ArrayList<>();
+    }
+
+    public abstract void displayCustomerInfo();
+
+    public void makePayment(PaymentStrategy paymentStrategy, double amount) {
+        paymentStrategy.pay(amount);
+        String paymentInfo = String.format("Amount %.2f - %s",
+                amount, paymentStrategy.getPaymentDetails());
+        paymentHistory.add(paymentInfo);
+    }
+
+    public void showPaymentHistory() {
+        System.out.println("Payment history for " + name + ":");
+        for (String payment : paymentHistory) {
+            System.out.println(payment);
+        }
+    }
 }
